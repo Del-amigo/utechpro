e054894cbcf45755e9b1a6c0be661335020d2685
Sule Taylanhan <rcptay@gmail.com>
2023-12-03 19:33:14 -0500

    updated contact info

--- src/components/contact/box-items.js
+++ src/components/contact/box-items.js
@@ -39,7 +39,7 @@ const BoxItems = () => {
                   <a href="mailto:utechproteam@gmail.com">utechproteam@gmail.com</a>
                 </p>
                 <p>
-                  <a href="tel:602-762-472-96">+(267) 266-7070</a>
+                  <a href="tel:602-762-472-96">+1 (267) 266-7070</a>
                 </p>
               </>
             }
